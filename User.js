28ac9b9df29b16aa3b4e88ed05ee7831e5268da5
GasinskiN <gasinskinorman@gmail.com>
2023-03-22 20:11:30 +0100

    Add hashing methods instead of encryption

--- User.js
+++ User.js
@@ -1,18 +1,25 @@
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const crypto = require("crypto");
 require('dotenv').config();
 const userSchema = new mongoose.Schema({
     username:{
         type: String,
         required: true
     },
-    password:{
-        type: String,
-        required: true
-    },
+    password: String,
+    salt: String,
 })
 
-const secret = process.env.MY_KEY;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
+
+userSchema.methods.setPassword = function() {
+    this.salt = crypto.randomBytes(16).toString("hex");
+    this.password = crypto.pbkdf2Sync(this.password, this.salt, 1000, 64, "sha512").toString("hex");
+}
+
+userSchema.methods.checkPasswordValidity = function(password) {
+    const hashedPassword = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
+    return this.password === hashedPassword;
+
+}
 
 module.exports = mongoose.model("User", userSchema);
