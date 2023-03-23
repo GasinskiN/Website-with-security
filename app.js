eebfb4d40b12b35e8b1c9475a465945b64c200fb
GasinskiN <gasinskinorman@gmail.com>
2023-03-23 19:12:48 +0100

    Replace authentication with bcrypt by using passport

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
 const User = require(__dirname + "/User");
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "i am aware that a lot of",
+    resave: false, 
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 async function connectMongoDB(){
     await mongoose.connect('mongodb://127.0.0.1:27017/loginDataDB');
@@ -25,28 +40,9 @@ app.route("/login")
     res.render("login");
 })
 .post(async function(req, res){
-    const username = req.body.username;
-    const plainTextPassword = req.body.password;
-
-    try {
-        const existingUser = await User.findOne({username: username});
-        if (existingUser === null) {
-            res.status(400).send("User not found");
-
-        } else{
-            bcrypt.compare(plainTextPassword, existingUser.password, function(err, isPasswordTheSame) {
-                if(isPasswordTheSame){
-                    res.render("secrets");
-                } 
-                else{
-                    res.status(400).send("Wrong password");
-                }
-            });
-        }
-
-    } catch (error) {
-        res.render(error.message);
-    }
+    passport.authenticate("local") (req, res, function(){
+        res.redirect("/secrets");
+    })
 
 });
 
@@ -57,23 +53,25 @@ app.route("/register")
 })
 
 .post(async function(req, res){
-    try {
-        bcrypt.hash(req.body.password, 11, async function(err, hash){
-            if(!err){
-            const newUser = new User({username: req.body.username})
-            newUser.password = hash;
-            await newUser.save();
-            res.render("secrets");
-            } else {
-                console.log(err);
-            }
-        });
-
-    } catch (error) {
-        res.send(error.message);
-    }   
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if (err) {
+            console.log(err);
+            res.redirect("register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            })
+        }
+    })
 });
 
+app.get("/secrets", function(req, res){
+    if (req.isAuthenticated()){
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+})
 
 app.listen(3000, function(){
     console.log("Server running on port 3000");
