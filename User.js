c6a1af133a3dd402364b2cd20e9b8b45e4cd8627
GasinskiN <gasinskinorman@gmail.com>
2023-03-24 20:22:59 +0100

    Remove unnecessary dotenv config

--- User.js
+++ User.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
-require('dotenv').config();
 const userSchema = new mongoose.Schema({
     username:{
         type: String,
         required: true
     },
     password: String,
+    googleId: String,
 })
-
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
